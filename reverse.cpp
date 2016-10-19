6e76f38c03957fbd06af2da80ed718228dc2fe6e
LightningFLash <12lightningflash12@gmail.com>
2016-10-19 13:12:25 -0400

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+  int number, reverse = 0;
+  
+  cout<< "Enter a number: ";
+  cin>> number >> endl;
+  
+  for( ; number != 0; ){
+    reverse = reverse * 10;
+    reverse = reverse + number % 10;
+    number = number / 10;
+  }
+  
+  cout<<"The reversed number is: "<< reverse << endl;
+  
+  return 0;
+}
