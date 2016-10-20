0bd3b3a541a5064c33e3fcd6228b10d745942f18
LightningFLash <12lightningflash12@gmail.com>
2016-10-20 13:51:13 -0400

    Update funWithLoops.cpp

--- funWithLoops.cpp
+++ funWithLoops.cpp
@@ -11,5 +11,34 @@ void a(){
 char x[20] = {"Compileing\ncompiled\n"};
 
 void b(){
-  
+  Sleep(200);
 }
+
+char y[11] = {"Loading...\n"};
+
+void c(){
+  Sleep(300);
+}
+
+char z[19] = {"Hello. How are you?"};
+
+int main(){
+  for(int l = 0; l < 20; l ++){
+    cout << x[l];
+    a();
+  }
+  
+  for(int m = 0; m < 11; m ++){
+    cout << y[m];
+    b();
+  }
+  
+  for(int n = 0; n < 19; n ++){
+    cout << z[n];
+    c(0;
+  }
+  
+  getch();
+  return 0;
+}
+
