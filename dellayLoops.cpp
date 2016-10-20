5db52eaae63366e93b921a3721270e7cf1539afc
LightningFLash <12lightningflash12@gmail.com>
2016-10-20 10:00:36 -0400

    Create dellayLoops.cpp

---
+++ dellayLoops.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void z(){
+  Sleep(500);
+}
+
+char x[6] = {'h', 'e', 'l', 'l', 'o', '!'};
+
+int main(){
+  for(int 1 = 0; i < 6; i ++){
+    cout << x[i];
+    z();
+  }
+  
+  getch();
+  return 0;
+}
+
