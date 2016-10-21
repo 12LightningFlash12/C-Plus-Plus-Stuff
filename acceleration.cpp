3738ad486e842c6f1d3e319ed6d084009f134d18
LightningFLash <12lightningflash12@gmail.com>
2016-10-21 10:51:36 -0400

    Update acceleration.cpp

--- acceleration.cpp
+++ acceleration.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+  double force, mass, time, cVel, accel, ch;
+  
+  cout << "|-----------------------|\n";
+  cout << "|Acceleration calculator|\n";
+  cout << "|-----------------------|\n\n\n";
+  
+  cout << "What are you using?\n";
+  cout << "1. Force and Mass or 2. time and Velocity\n";
+  cin >> ch;
+  cout << "\n\n";
+  
+  
+}
