53fdc293003be7b1200b6e3a52cc5700123aeec0
LightningFlash <12lightningflash12@gmail.com>
2016-10-24 14:57:45 -0400

    Update stockMarket.cpp

--- stockMarket.cpp
+++ stockMarket.cpp
@@ -6,6 +6,23 @@ int main(int argc, char ** argv){
   
   cout<< "Enter name: ";
   cin >> name;
+  cout << "\n";
   
+  cout << "Enter nuumber of shares: ";
+  cin >> share;
+  cout << "\n";
   
+  cout << "Enter share value: ";
+  cin >> val;
+  cout << "\n";
+  
+  total = share * val;
+  
+  cout << "Stock name: " << name << "\n" << endl;
+  cout << "Number of shares: " << share << "\n" << endl;
+  cout << "Share value: " << val << "\n" << endl;
+  cout << "Total value: " << total << "\n" << endl;
+  
+  return 0;
 }
+
